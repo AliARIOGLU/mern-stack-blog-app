d3e9b934e52431c8abc7b2e498b12d5f7937a292
Ali <ali.arioglu06@gmail.com>
2024-03-23 16:34:18 +0300

    update

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -2,4 +2,5 @@ export const headerLinks = [
   { id: 1, title: "Home", path: "/" },
   { id: 2, title: "About", path: "/about" },
   { id: 3, title: "Projects", path: "/projects" },
+  { id: 4, title: "Profile", path: "/dashboard?tab=profile" },
 ];
